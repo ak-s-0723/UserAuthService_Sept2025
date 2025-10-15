5b56ace43d5a999d43fbd483d1de73e025cf919f
khannaanurag1 <khannaanurag1@gmail.com>
2025-10-15 07:00:46 +0530

    first commit

---
+++ src/main/java/org/example/userauthservice_sept2025/UserAuthServiceSept2025Application.java
@@ -0,0 +1,13 @@
+package org.example.userauthservice_sept2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthServiceSept2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthServiceSept2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/userauthservice_sept2025/UserAuthServiceSept2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.userauthservice_sept2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceSept2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
